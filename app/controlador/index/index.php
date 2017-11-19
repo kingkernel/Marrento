535103573f24cc4d44f60b1ed26bb44268100bc4
Daniel José dos Santos <daniel.santos.ap@gmail.com>
2017-11-19 08:48:54 -0200

    para tabelas

--- app/controlador/index/index.php
+++ app/controlador/index/index.php
@@ -1,46 +1,11 @@
 <?php
+
 class index {
 	public function __construct(){
 		
 	}
 	public function index(){
 		echo "Ola mundo Marrento!!!";
-	}
-}
-
-class load_table{
-	public $table;
-	public $primarykey;
-
-	public function __construct(){
-
-	}
-	public function conectdb(){
-
-	}
-	public function create_insert(){
-
-	}
-	public function create_delete(){
-
-	}
-	public function create_update(){
-
-	}
-	public function query_full(){
-
-	}
-	public function query_simple(){
-
-	}
-	public function create_procedure_add(){
-
-	}
-	public function create_procedure_del(){
-		
-	}
-	public function create_procedure_sel(){
-		
 	}
 }
 ?>
---
+++ app/motor/class/native/load_table.class.php
@@ -0,0 +1,37 @@
+<?php
+class load_table{
+	public $table;
+	public $primarykey;
+
+	public function __construct(){
+
+	}
+	public function conectdb(){
+
+	}
+	public function create_insert(){
+
+	}
+	public function create_delete(){
+
+	}
+	public function create_update(){
+
+	}
+	public function query_full(){
+
+	}
+	public function query_simple(){
+
+	}
+	public function create_procedure_add(){
+
+	}
+	public function create_procedure_del(){
+		
+	}
+	public function create_procedure_sel(){
+		
+	}
+}
+?>
