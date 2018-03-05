510bc95f8a55e8b557627ba600181da2c6c007a0
Daniel Santos <daniel.santos.ap@gmail.com>
2018-03-05 00:08:02 -0300

    update

---
+++ app/motor/class/native/back-end/notifyServer.class.php
@@ -0,0 +1,18 @@
+<?php
+/*
+*Data de Criação: 04/03/2018
+*
+*/
+class notifyServer {
+	public function __construct(){
+
+	}
+	public function push(){
+		$getData = $_GET;
+		print_r(expression)
+	}
+	public function pop(){
+
+	}
+}
+?>
---
+++ public/js/this/backend/notifyVerify.js
@@ -0,0 +1,7 @@
+/*
+
+*/
+function getMorf(dna, objConten, callback){
+	var dnaStr = dna.split("@dna", dna);
+	console.log(dnaStr);
+}
