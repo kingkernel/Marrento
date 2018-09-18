43672dda7000e39324c4a020de416c5ad49275c4
Daniels <daniel.santos.ap@gmail.com>
2018-09-18 11:04:58 -0300

    las

--- kingkernel_banco.sql
+++ kingkernel_banco.sql
@@ -1,7 +1,7 @@
 create database kingkernel character set=utf8;
 use kingkernel;
 
-create table logs(
+create table log(
 id bigint auto_increment,
 acao text,
 diaevento datetime,
@@ -12,3 +12,10 @@ create table url(
 id bigint auto_increment,
 url varchar(64),
 primary key(id))engine=innodb charset=utf8;
+
+delimiter //
+	create procedure sp_add_log(arg_acao text, arg_diaevento datetime, arg_info text)
+		begin
+			insert into log(acao, diaevento, info) values (arg_acao, arg_diaevento, arg_info);
+		end //
+delimiter ;
