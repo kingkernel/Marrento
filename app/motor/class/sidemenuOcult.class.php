f132f238d6209e6666f946274b9dd96ede21578a
Daniel José dos Santos <daniel.santos.ap@gmail.com>
2019-03-06 17:51:28 -0300

    preparativos
    preparando para novo menu

--- app/motor/class/sidemenuOcult.class.php
+++ app/motor/class/sidemenuOcult.class.php
@@ -82,13 +82,13 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="nav navbar-nav ml-auto">
                             <li class="nav-item active">
-                                <a class="nav-link" href="#">Page</a>
+                                <a class="nav-link" href="#">login</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Page</a>
+                                <a class="nav-link" href="#">empresas</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Page</a>
+                                <a class="nav-link" href="#">pessoas</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#">Page</a>
---
+++ app/motor/class/ulList.class.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Data criação: 06/03/2019
+Autor: Daniel J. Santos
+*/
+class ulList {
+	public $liItens = [];
+	public $class;
+	public $id;
+
+	public function __construct(){
+		
+	}
+}
+
+?>
