8f34270ca0bc30c4b4ae59595a375badad447159
Daniel José dos Santos <daniel.santos.ap@gmail.com>
2019-03-18 18:09:31 -0300

    new feactures

--- app/motor/class/avisoSimples.class.php
+++ app/motor/class/avisoSimples.class.php
@@ -1,8 +1,12 @@
 <?php
+/*/
+Data criação: 18/03/1029
+Autor: Daniel J. Santos
+*/
 class avisoSimples{
 	public $titleSize = "2";
 	public $title;
-	public $text;
+	public $text; 
 	public $content;
 	public function __construct(){
 
@@ -11,7 +15,7 @@ class avisoSimples{
 		echo '<h'.$this->titleSize.'>'.$this->title.'</h'.$this->titleSize.'><p>'.$this->text.'</p>';
 	}
 	public function html(){
-		$this->content = '<h'.$this->titleSize.'>'.$this->title.'</h'.$this->titleSize.'><p>'.$this->text.'</p>';
+		$this->content='<h'.$this->titleSize.'>'.$this->title.'</h'.$this->titleSize.'><p>'.$this->text.'</p>';
 		return $this->content;
 	}
 }
