4cfda63f2dfefba3eff3123c41b11288196951d5
daniels <daniel.santos.ap@gmail.com>
2019-03-22 20:17:43 -0300

    up

--- app/motor/class/fieldsetPanel.class.php
+++ app/motor/class/fieldsetPanel.class.php
@@ -1,10 +1,19 @@
 <?php
 class fieldsetPanel{
+	public $formAction = "";
+	public $formId = "";
+	public $fomItens = [];
 	public function __construct(){
 
 	}
-	public function (){
-		
+	public function render(){
+		echo ' <form class="well form-horizontal" action="'.$this->formAction.'" method="POST" id="'.$this->formId.'"><fieldset><legend>'.$this->fieldsetLegend.'</legend>';
+		foreach ($this->formItens as $key => $value) {
+			$value->render(); 
+		};
+                        $body .= $campo1->html().$campo2->html().$campo3->html().$campo4->html().$campo5->html().$campo6->html().$campo7->html().$campo8->html().$campo9->html(); 
+                      $body .='</fieldset>
+                   </form>';
 	}
 }
 ?>
