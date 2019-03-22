df6534931607d6824adc3826f8d2dd52dce81fe0
Daniel José dos Santos <daniel.santos.ap@gmail.com>
2019-03-22 08:09:51 -0300

    front

--- app/modelo/index_modelo/indexOn.php
+++ app/modelo/index_modelo/indexOn.php
@@ -27,74 +27,25 @@ $body = '<div class="container">
                         $campo7 = new formChildItem;
                         $campo8 = new formChildItem;
                         $campo9 = new formChildItem;
-                        $body .= $campo1->html().$campo2->html().$campo3->html().$campo4->html().$campo5->html().$campo6->html().$campo7->html().$campo8->html().$campo9->html();;
+                        $body .= $campo1->html().$campo2->html().$campo3->html().$campo4->html().$campo5->html().$campo6->html().$campo7->html().$campo8->html().$campo9->html();
                         $body .='
                       </fieldset>
                    </form>
                 </td>
                 <td colspan="1">
                    <form class="well form-horizontal">
-                      <fieldset><legend>Chamados Técnicos</legend>
-                         <div class="form-group">
-                            <label class="col-md-4 control-label">Full Name</label>
-                            <div class="col-md-8 inputGroupContainer">
-                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span><input id="fullName" name="fullName" placeholder="Full Name" class="form-control" required="true" value="" type="text"></div>
-                            </div>
-                         </div>
-                         <div class="form-group">
-                            <label class="col-md-4 control-label">Address Line 1</label>
-                            <div class="col-md-8 inputGroupContainer">
-                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="addressLine1" name="addressLine1" placeholder="Address Line 1" class="form-control" required="true" value="" type="text"></div>
-                            </div>
-                         </div>
-                         <div class="form-group">
-                            <label class="col-md-4 control-label">Address Line 2</label>
-                            <div class="col-md-8 inputGroupContainer">
-                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="addressLine2" name="addressLine2" placeholder="Address Line 2" class="form-control" required="true" value="" type="text"></div>
-                            </div>
-                         </div>
-                         <div class="form-group">
-                            <label class="col-md-4 control-label">City</label>
-                            <div class="col-md-8 inputGroupContainer">
-                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="city" name="city" placeholder="City" class="form-control" required="true" value="" type="text"></div>
-                            </div>
-                         </div>
-                         <div class="form-group">
-                            <label class="col-md-4 control-label">State/Province/Region</label>
-                            <div class="col-md-8 inputGroupContainer">
-                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="state" name="state" placeholder="State/Province/Region" class="form-control" required="true" value="" type="text"></div>
-                            </div>
-                         </div>
-                         <div class="form-group">
-                            <label class="col-md-4 control-label">Postal Code/ZIP</label>
-                            <div class="col-md-8 inputGroupContainer">
-                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="postcode" name="postcode" placeholder="Postal Code/ZIP" class="form-control" required="true" value="" type="text"></div>
-                            </div>
-                         </div>
-                         <div class="form-group">
-                            <label class="col-md-4 control-label">Country</label>
-                            <div class="col-md-8 inputGroupContainer">
-                               <div class="input-group">
-                                  <span class="input-group-addon" style="max-width: 100%;"><i class="glyphicon glyphicon-list"></i></span>
-                                  <select class="selectpicker form-control">
-                                     <option>A really long option to push the menu over the edget</option>
-                                  </select>
-                               </div>
-                            </div>
-                         </div>
-                         <div class="form-group">
-                            <label class="col-md-4 control-label">Email</label>
-                            <div class="col-md-8 inputGroupContainer">
-                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span><input id="email" name="email" placeholder="Email" class="form-control" required="true" value="" type="text"></div>
-                            </div>
-                         </div>
-                         <div class="form-group">
-                            <label class="col-md-4 control-label">Phone Number</label>
-                            <div class="col-md-8 inputGroupContainer">
-                               <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span><input id="phoneNumber" name="phoneNumber" placeholder="Phone Number" class="form-control" required="true" value="" type="text"></div>
-                            </div>
-                         </div>
-                      </fieldset>
+                      <fieldset><legend>Chamados Técnicos</legend>';
+                        $campo1 = new formChildItem;
+                        $campo2 = new formChildItem;
+                        $campo3 = new formChildItem;
+                        $campo4 = new formChildItem;
+                        $campo5 = new formChildItem;
+                        $campo6 = new formChildItem;
+                        $campo7 = new formChildItem;
+                        $campo8 = new formChildItem;
+                        $campo9 = new formChildItem;
+                        $body .= $campo1->html().$campo2->html().$campo3->html().$campo4->html().$campo5->html().$campo6->html().$campo7->html().$campo8->html().$campo9->html(); 
+                      $body .='</fieldset>
                    </form>
                 </td>
             </tr>
