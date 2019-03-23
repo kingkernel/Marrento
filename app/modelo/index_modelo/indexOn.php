8462dc6c7b7adce17671cc1f21bf128edd2ce141
daniels <daniel.santos.ap@gmail.com>
2019-03-22 22:55:17 -0300

    new

--- app/modelo/index_modelo/indexOn.php
+++ app/modelo/index_modelo/indexOn.php
@@ -9,60 +9,29 @@ $page->headersinclude .= fontAwesome(urlcss($_GET));
 		$use_info->exitlink = "/auth/logout/";
 		$use_info->updataidlink = "/user/update/";
 
-  $topmenu->itensright=[$use_info];
-  $body = '<div class="container">
-       <table class="table">
-          <tbody>
-             <tr>
-                <td colspan="1">
-                   <form class="well form-horizontal">
-                      <fieldset><legend>Fila de Chamados</legend>';
-                        $campo1 = new formChildItem;
-                        $campo2 = new formChildItem;
-                        $campo3 = new formChildItem;
-                        $campo4 = new formChildItem;
-                        $campo5 = new formChildItem;
-                        $campo6 = new formChildItem;
-                        $campo7 = new formChildItem;
-                        $campo8 = new formChildItem;
-                        $campo9 = new formChildItem;
-                        $body .= $campo1->html().$campo2->html().$campo3->html().$campo4->html().$campo5->html().$campo6->html().$campo7->html().$campo8->html().$campo9->html();
-                        $body .='
-                      </fieldset>
-                   </form>
-                </td>
-                <td colspan="1">';
-                    $panel_1 = new fieldsetPanel;
-                    $panel_1->formAction = "ativa/";
-                    $panel_1->formItens = [$campo1, $campo2, $campo3, $campo4, $campo5, $campo6, $campo7, $campo8, $campo9];
-                    $body .= $panel_1->html();
-                $body .='</td>
-            </tr>
-          </tbody>
-       </table>
-    </div>';
+    $campo1 = new formChildItem;
+    $campo2 = new formChildItem;
+    $campo3 = new formChildItem;
+    $campo4 = new formChildItem;
+    $campo5 = new formChildItem;
+    $campo6 = new formChildItem;
+    $campo7 = new formChildItem;
+    $campo8 = new formChildItem;
+    $campo9 = new formChildItem;
 
+  $topmenu->itensright=[$use_info];
+  $body = '<div class="container"><table class="table"><tbody><tr><td colspan="1">';
+    $panel_0 = new fieldsetPanel;
+    $panel_0->formAction = "ativa/";
+    $panel_0->formItens = [$campo1, $campo2, $campo3, $campo4, $campo5, $campo6, $campo7, $campo8, $campo9];
+    $body .= $panel_0->html();
+$body .= '</td><td colspan="1">';
+    $panel_1 = new fieldsetPanel;
+    $panel_1->formAction = "ativa/";
+    $panel_1->formItens = [$campo1, $campo2, $campo3, $campo4, $campo5, $campo6, $campo7, $campo8, $campo9];
+    $body .= $panel_1->html();
+    $body .='</td></tr></tbody></table></div>';
 
 $page->bodycontent = $topmenu->html().$body;
 $page->render();
-/*
-	[estilo] => default
-	[brand] => site
-    [brandLink] => /
-    [itensleft] => Array
-        (
-        )
-    [itensright] => Array
-        (
-        )
-    [id] => barra-navegacao-1
-    [navextras] => 
-    [nav_ul_left] => 
-    [nav_ul_right] => 
-
-$pathvisao = PATHVISAO . get_class(). "_visao/" . get_class() . "_index.html";
-$pagina = compactada($pathvisao);
-$pagina = str_replace("[@pathpublic]", "public/", $pagina);
-print_r($pagina);
-*/
 ?>
