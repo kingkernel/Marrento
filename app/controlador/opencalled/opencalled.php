e26f98b5648b4686e01f9acf46f34d3bbe30b0b3
daniels <daniel.santos.ap@gmail.com>
2019-03-24 03:31:00 -0300

    up

--- app/controlador/opencalled/opencalled.php
+++ app/controlador/opencalled/opencalled.php
@@ -1,11 +1,13 @@
 <?php
 class opencalled{
 	public function __construct(){
-
+		$page = new page;
+		$this->page = $page;
+		return $this->page;
 	}
 	public function index(){
 		$sql = 'call sp_add_teccalled('.$_POST["prob"].', '.$_POST["userid"].', "'.$_POST["descri"].'")';
-		echo $sql;
+		$this->page->render();
 	}
 }
 ?>
