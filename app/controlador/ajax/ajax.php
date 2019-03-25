49eff8d7c1c0f99e9667499fd285f47dfad2f5e5
Daniel José dos Santos <daniel.santos.ap@gmail.com>
2019-03-25 12:58:50 -0300

    up

--- app/controlador/ajax/ajax.php
+++ app/controlador/ajax/ajax.php
@@ -4,7 +4,23 @@ class ajax {
 
 	}
 	public function loadTableCalled(){
-		
+	$panel_0 = new rowAlert;
+    $panel_0->colSize = "md-12";
+    $panel_0->class = "alert alert-warning";
+    $panel_0->titleSize = "3";
+    $panel_0->alertTitle = 'Chamados Abertos <span class="glyphicon glyphicon-tasks"></span>';
+    
+    $table = new table;
+    $headers = ["Usuário", "Problema", "Estatus"];
+    $table->headers($headers);
+    $sql = queryDb("call sp_sel_teccalled()");
+    $arrayRow = [];
+    while ($query = $sql->fetch(PDO::FETCH_ASSOC)) {
+      if($query["estatus"] == "Aberto"){$class = "warning";} else {$class = "success";};
+        array_push($arrayRow, '<tr><td>'.$query["nameperson"].'</td><td>'.$query["prob"].'</td><td><span class="label label-'.$class.'">'.$query["estatus"].'</span></td></tr>');
+      };
+    $table->rows($arrayRow);
+    $table->html();
 	}
 }
 ?>
