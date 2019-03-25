e607d2c8dffa1b2099d6bbec8c9b0825284dfc75
Daniel José dos Santos <daniel.santos.ap@gmail.com>
2019-03-25 16:51:35 -0300

    up

--- public/js/this/backend/xamastiFunctions.js
+++ public/js/this/backend/xamastiFunctions.js
@@ -1,16 +1,13 @@
 /*
 Data de criação: 25/03/2019
 Autor: Daniel J. Santos
-@divid
-@path_to_file
-@params
+@divid:  id que será aplicado o ajax
+@path_to_file: localização do serviço
+@params: parametros adicionais na requisição
 */
 function loadTableCalled(){
 	$("@divid").load('@path_to_file',{
-	@params} ,
-		function(){
-		/* Stuff to do after the page is loaded */
+	@params
 	});
-	
 }
 setInterval("loadTableCalled()", 1000);
