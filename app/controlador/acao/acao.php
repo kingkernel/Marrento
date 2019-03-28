2a37c5a87f7d1223f94b732a94c3c9169911d03f
Daniel José dos Santos <daniel.santos.ap@gmail.com>
2019-03-28 10:34:05 -0300

    up

--- app/controlador/acao/acao.php
+++ app/controlador/acao/acao.php
@@ -12,12 +12,32 @@ class acao {
 
 	}
 	public function chamados(){
-		$sql = "call sp_sel_teccalled()";
-		$query = queryDb($sql);
-		$dados =$query->fetch(PDO::FETCH_ASSOC);
-		//print_r($dados);
-		// [id] => 1 [nameperson] => root [prob] => Impressora não imprime [estatus] => Aberto [opencalled] => 2019-03-25 10:56:51 [description] => Preciso imprimir relatórios
-		$table = new table;
+		if ($_GET["urldigitada"]==get_class()."/"){
+			echo "redundante";
+		};
+	$panel_0 = new rowAlert;
+    $panel_0->colSize = "md-12";
+    $panel_0->class = "alert alert-warning";
+    $panel_0->titleSize = "3";
+    $panel_0->alertTitle = 'Chamados Abertos <span class="glyphicon glyphicon-tasks"></span>';
+    
+    $table = new table;
+    $headers = ["Usuário", "Problema", "Estatus", "Ação"];
+    $table->headers($headers);
+    $sql = queryDb("call sp_sel_teccalled()");
+    $arrayRow = [];
+    while ($query = $sql->fetch(PDO::FETCH_ASSOC)) {
+      if($query["estatus"] == "Aberto"){$class = "warning";} else {$class = "success";};
+        array_push($arrayRow, '<tr><td>'.$query["nameperson"].'</td><td>'.$query["prob"].'</td><td><span class="label label-'.$class.'">'.$query["estatus"].'</span></td><td><form action="../updatecalled/" method="post"><select required="true"><option value="">escolha</option><option value="Em Atendimento">Em atendimento</option><option value="Encerrado">Encerrado</option></select> <input type="submit" class="btn btn-success" value="Atualizar"></form></td></tr>');
+      };
+    $table->rows($arrayRow);
+
+    $panel_0->alertText = $table->html();
+    $sql = 'call sp_sel_teccalled()';
+    $query = queryDb($sql);
+    $dados =$query->fetch(PDO::FETCH_ASSOC);
+
+		$this->page->bodycontent = $panel_0->html();
 		$this->page->render();
 	}
 }
